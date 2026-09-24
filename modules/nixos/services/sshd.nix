e15cbafe86a6b8309a1ac4b214982e61e4cfb75a
XardecQs <126134158+XardecQs@users.noreply.github.com>
2026-09-24 09:42:31 -0500

    feat(sshd): opcion passwordAuthentication (deshabilitada por defecto)

--- modules/nixos/services/sshd.nix
+++ modules/nixos/services/sshd.nix
@@ -10,12 +10,21 @@ in
 {
   options.modulos.nixos.services.sshd = {
     enable = lib.mkEnableOption "sshd";
+    passwordAuthentication = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Permitir autenticación por contraseña en SSH. Requiere además clave/password del usuario.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     services.openssh = {
       enable = true;
-      settings.PermitRootLogin = "no";
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = cfg.passwordAuthentication;
+        KbdInteractiveAuthentication = cfg.passwordAuthentication;
+      };
     };
 
     environment.systemPackages = with pkgs; [
